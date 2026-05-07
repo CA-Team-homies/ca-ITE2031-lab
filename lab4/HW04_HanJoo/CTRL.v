9851345f2b60a7d2c7ea375877cef74c26ec018d
norvakent <hjlee100679@gmail.com>
2026-05-07 22:31:09 +0900

    udpate state 0, 3, 4.

--- lab4/HW04_HanJoo/CTRL.v
+++ lab4/HW04_HanJoo/CTRL.v
@@ -33,14 +33,37 @@ module CTRL(
 		// FIXME
 		case (State)
 			`STATE0: begin
+				IorD = 0;
+				IRWrite = 1;
+				ALUSrcA = 0;
+				ALUSrcB = 2'b01;
+				PCWrite = 1;
+				PCSource = 2'b00;
+				NextState = `STATE1;
 			end
 			`STATE1: begin
 			end
 			`STATE2: begin
 			end
 			`STATE3: begin
+				IorD = 1;
+				if (opcode == `OP_LW) NextState = `STATE4;
+				else if (opcode == `OP_SW) begin
+					MemWrite = 1;
+					NextState = `STATE0;
+				end
 			end
 			`STATE4: begin
+				RegDst = 0;
+				RegWrite = 1;
+				MemtoReg = 0;
+				NextState = `STATE0;
+				if (opcode == `OP_RTYPE) begin
+					RegDst = 1;
+				end
+				else begin
+					if (opcode == `OP_LW) MemtoReg = 1;
+				end
 			end
 		endcase
 	end
