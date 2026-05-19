89622295e0853a82b93f249fe99bdeb07ee9294b
norvakent <hjlee100679@gmail.com>
2026-05-19 16:06:21 +0900

    a

--- lab5/HW05_HanJoo/CPU.v
+++ lab5/HW05_HanJoo/CPU.v
@@ -11,20 +11,60 @@ module CPU(
 	reg [31:0] PC;
 
 	// Microarchitectural Registers
-	reg [31:0] PC_next;
-	reg [31:0] IR;
-	reg [31:0] MDR;
-	reg [31:0] A;
-	reg [31:0] B;
-	reg [31:0] ALUOut;
-	reg [2:0] State;
+	// IF/ID
+	reg [31:0] IFID_PC;
+	reg [31:0] IFID_Inst;
 
-	reg [4:0] wr_addr;
-	reg [31:0] wr_data;
-	reg [31:0] Address;
-	reg [31:0] Operand1;
-	reg [31:0] Operand2;
-	
+	// ID/EX
+	reg IDEX_RegWrite;
+	reg [1:0] IDEX_MemtoReg;
+
+	reg IDEX_MemWrite;
+
+	reg [1:0] IDEX_PCSource;
+	reg IDEX_SignExtend;
+	reg IDEX_ALUSrc;
+	reg [3:0] IDEX_ALUOp;
+	reg [1:0] IDEX_RegDst;
+
+	reg [31:0] IDEX_PC;
+	reg [31:0] IDEX_Data1;
+	reg [31:0] IDEX_Data2;
+	reg [25:0] IDEX_IMMJ;
+	reg [15:0] IDEX_IMMI; // resolve @ EX stage
+	reg [4:0] IDEX_RT;
+	reg [4:0] IDEX_RD;
+
+	// EX/MEM
+	reg EXMEM_RegWrite;
+	reg [1:0] EXMEM_MemtoReg;
+
+	reg EXMEM_MemWrite;
+
+	reg [31:0] EXMEM_PC;
+	reg [31:0] EXMEM_ALUResult;
+	reg [31:0] EXMEM_Data;
+	reg [31:0] EXMEM_RegAddr;
+
+	// MEM/WB
+	reg MEMWB_RegWrite;
+	reg [1:0] MEMWB_MemtoReg;
+
+	reg [31:0] MEMWB_PC;
+	reg [31:0] MEMWB_Data;
+	reg [31:0] MEMWB_ALUResult;
+
+	// define variables
+	reg [31:0] PC_next;
+	reg [31:0] ext_imm;
+	reg [4:0] RegAddr;
+	reg [31:0] RegData;
+	reg [31:0] operand2;
+
+	// define wires
+	wire [31:0] inst_addr;
+	wire [31:0] inst;
+
 	// Split the Instructions
 	wire [5:0] opcode;
 	wire [4:0] rs;
@@ -39,99 +79,104 @@ module CPU(
 	wire [4:0] rd_addr2;
 	wire [31:0] rd_data1;
 	wire [31:0] rd_data2;
+
+	wire [31:0] inst_addr;
+	wire [31:0] inst;
 	wire [31:0] mem_write_data;
 	wire [31:0] mem_read_data;
-	//wire [31:0] r31;
+	wire [31:0] mem_addr;
 
+
+	wire [31:0] operand1;
+	wire [31:0] operand2;
 	wire [31:0] alu_result;
 
 	// Control-related wires
-	wire [1:0] RegDst;
 	wire RegWrite;
 	wire [1:0] MemtoReg;
 	wire MemWrite;
-	wire IorD;
-	wire SignExtend;
-	wire ALUSrcA;
-	wire [1:0] ALUSrcB;
-	wire [3:0] ALUOp;
 	wire [1:0] PCSource;
-	wire PCWriteCond;
-	wire PCWrite;
-	wire [2:0] NextState;
-	wire IRWrite;
-	wire InstDone;
-
+	wire SignExtend;
+	wire ALUSrc;
+	wire [3:0] ALUOp;
+	wire [1:0] RegDst;
 
 	// Define the wires
-	assign opcode = IR[31:26];
-	assign rs = 		IR[25:21];
-	assign rt = 		IR[20:16];
-	assign rd = 		IR[15:11];
-	assign shamt = 	IR[10:6];
-	assign funct = 	IR[5:0];
-	assign immi = 	IR[15:0];
-	assign immj = 	IR[25:0];
-
-  	assign rd_addr1 = rs;
-  	assign rd_addr2 = rt;
-
-	assign mem_write_data = B;
+	assign opcode = IFID_Inst[31:26];
+	assign rs = 		IFID_Inst[25:21];
+	assign rt = 		IFID_Inst[20:16];
+	assign rd = 		IFID_Inst[15:11];
+	assign shamt = 	IFID_Inst[10:6];
+	assign funct = 	IFID_Inst[5:0];
+	assign immi = 	IFID_Inst[15:0];
+	assign immj = 	IFID_Inst[25:0];
 
 	assign halt	= (IR == 32'b0);
 	
 	always @(*) begin
-		Address = IorD ? ALUOut : PC;
+		ext_imm = IDEX_SignExtend ? {{16{immi[15]}}, IDEX_IMMI} : {16'b0, IDEX_IMMI};
+		case (PCSource)
+			2'd0: PC_next = PC + 4;
+			2'd1: PC_next = PC + 4 + (((opcode == `OP_BEQ && IDEX_Data1 == IDEX_Data2) || (opcode == `OP_BNE && IDEX_Data1 != IDEX_Data2)) ? ext_imm : 0);
+			2'd2: PC_next = ((PC + 4) & 32'hF0000000) | (IDEX_IMMJ << 2);
+			2'd3: PC_next = IDEX_Data1;
+		endcase
 		case (RegDst)
-			2'd0: wr_addr = rt;
-			2'd1: wr_addr = rd;
-			2'd2: wr_addr = 5'd31;
+			2'd0: RegAddr = rt;
+			2'd1: RegAddr = rd;
+			2'd2: RegAddr = 5'd31;
 		endcase
 		case (MemtoReg)
-			2'd0: wr_data = ALUOut;
-			2'd1: wr_data = MDR;
-			2'd2: wr_data = PC;
+			2'd0: RegData = MEMWB_Data;
+			2'd1: wr_data = MEMWB_ALUResult;
+			2'd2: wr_data = MEMWB_PC;
 		endcase
-		Operand1 = ALUSrcA ? A : PC;
-		case (ALUSrcB)
-			2'd0: Operand2 = B;
-			2'd1: Operand2 = 32'd4;
-			2'd2: Operand2 = SignExtend ? {{16{immi[15]}}, immi} : {16'b0, immi};
-			2'd3: Operand2 = {{16{immi[15]}}, immi} << 2;
-		endcase
-		case (PCSource)
-			2'd0: PC_next = alu_result;
-			2'd1: PC_next = ALUOut;
-			2'd2: PC_next = (PC & 32'hF0000000) | (immj << 2);
-			//2'd3: PC_next = A;
-			2'd3: PC_next = rd_data1;
-		endcase
+		operand2 = ALUSrc ? ext_imm : IDEX_Data2;
 	end
 
 	// Update the Clock
 	always @(posedge clk) begin
 		if (rst) begin
 			PC <= 0;
-			IR <= 0;
-			MDR <= 0;
-			A <= 0;
-			B <= 0;
-			ALUOut <= 0;
-			State <= `STATE0;
 		end
 		else begin
-			if (PCWrite || (PCWriteCond && (alu_result == 0))) begin
-				PC <= PC_next;
-			end
-			if (IRWrite) begin
-				IR <= mem_read_data;
-			end
-			// always latched
-			MDR <= mem_read_data;
-			A <= rd_data1;
-			B <= rd_data2;
-			ALUOut <= alu_result;
-			State <= NextState;
+			// j, jr은 EX에서 immj, rddata1 EXstage에 점프추가
+			// Branch는 MEM에서 점프
+			// jal은 WB까지 가야함 점프는 EX에서 미리하되 HAZARD 신경쓰기
+			// 시그널 다시 설정후 삼항연산자사용
+			PC <= PC_next;
+			IFID_PC <= PC + 4;
+			IFID_Inst <= inst;
+
+			IDEX_RegWrite <= RegWrite;
+			IDEX_MemtoReg <= MemtoReg;
+			IDEX_MemWrite <= MemWrite;
+			IDEX_PCSource <= PCSource;
+			IDEX_SignExtend <= SignExtend;
+			IDEX_ALUSrc <= ALUSrc;
+			IDEX_ALUOp <= ALUOp;
+			IDEX_RegDst <= RegDst;
+			IDEX_PC <= IFID_PC;
+			IDEX_Data1 <= rd_data1;
+			IDEX_Data2 <= rd_data2;
+			IDEX_IMMI <= immi;
+			IDEX_IMMJ <= immj;
+			IDEX_RT <= rt;
+			IDEX_RD <= rd;
+
+			EXMEM_RegWrite <= IDEX_RegWrite;
+			EXMEM_MemtoReg <= IDEX_MemtoReg;
+			EXMEM_MemWrite <= IDEX_MemWrite;
+			EXMEM_PC <= IDEX_PC;
+			EXMEM_ALUResult <= alu_result;
+			EXMEM_Data <= IDEX_Data2;
+			EXMEM_RegAddr <= RegAddr;
+			
+			MEMWB_RegWrite <= EXMEM_RegWrite;
+			MEMWB_MemtoReg <= EXMEM_MemtoReg;
+			MEMWB_PC <= EXMEM_PC;
+			MEMWB_Data <= mem_read_data;
+			MEMWB_ALUResult <= EXMEM_ALUResult;
 		end	
 	end
 	
